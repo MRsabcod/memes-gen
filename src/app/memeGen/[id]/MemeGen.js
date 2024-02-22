9c8c9b5f3baa2c29fa04ab63886608657f1f3bd4
MRsabcod <sabeebr97@gmail.com>
2024-02-22 19:00:08 +0500

    commit

--- src/app/memeGen/[id]/MemeGen.js
+++ src/app/memeGen/[id]/MemeGen.js
@@ -63,7 +63,7 @@ setOpenModal(true)
     <input onChange={(e)=>{settext2(e.target.value)}} type="text" id="large-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
 </div> */}
 {
-    [...Array(searchmeme.box_count)].map((e, i) => {return <div className="mb-6">
+    [...Array(searchmeme.box_count)].map((e, i) => {return <div className="mb-6" key={`div${i}`}>
     <label key={`label${i}`} for="large-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Text {i+1}</label>
     <input key={`textbox${i}`} onChange={(e)=>{boxlst[i](e.target.value)}} type="text" id="large-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
 </div> })
